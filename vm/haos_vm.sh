e61fe31d0460f703234cc7de5229bae73d6f86d2
Erlend Fosseide Sellie <erlendsellie@gmail.com>
2023-01-09 15:50:57 +0100

    Default 8GB VM Size for HA VM

--- vm/haos_vm.sh
+++ vm/haos_vm.sh
@@ -258,7 +258,7 @@ pvesm alloc $STORAGE $VMID $DISK0 128 1>&/dev/null
 qm importdisk $VMID ${FILE%.*} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF},size=128K \
-  -scsi0 ${DISK1_REF},size=32G >/dev/null
+  -scsi0 ${DISK1_REF},size=8G >/dev/null
 qm set $VMID \
   -boot order=scsi0 >/dev/null
 qm set $VMID -description "# Home Assistant OS
